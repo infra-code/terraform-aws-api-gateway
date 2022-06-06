3bcbc6b9d1f5300c563c8e82246811f21b178b30
Lee Hull <lhull@exzeo.com>
2022-06-06 00:26:59 -0400

    Changes unrelated to a JIRA task

--- route53.tf
+++ route53.tf
@@ -8,8 +8,8 @@ resource "aws_route53_record" "main" {
   type    = "A"
 
   alias {
-    name                   = aws_api_gateway_domain_name.api.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
+    name                   = aws_api_gateway_domain_name.api[0].regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api[0].regional_zone_id
     evaluate_target_health = true
   }
 }
